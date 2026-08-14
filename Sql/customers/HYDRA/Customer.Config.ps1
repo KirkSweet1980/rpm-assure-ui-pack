d9425c993de2d237e795adc25a011023f02d4887
KirkSweet1980 <kirk.sweet@thesweets.co.za>
2026-08-14 04:37:45 +0000

    Hydrasales: Windows auth HYDRASRV\RPMAdmin, instance HydraSRV

--- Sql/customers/HYDRA/Customer.Config.ps1
+++ Sql/customers/HYDRA/Customer.Config.ps1
@@ -1,7 +1,7 @@
-# HYDRA / Hydrasales - InstanceName must match this SQL host (@@SERVERNAME).
+# HYDRA / Hydrasales - SQL host HydraSRV (192.168.196.1)
 $CustomerCode = 'HYDRA'
 $DisplayName = 'Hydrasales'
-$InstanceName = 'HYDRASRV'
+$InstanceName = 'HydraSRV'
 $LocalSqlUser = 'rpmassure'
 $LocalSqlPassword = '@ssuR3me!'
 $CentralSqlUser = 'rpmassure'
@@ -10,3 +10,14 @@ $CentralDataSource = '102.222.21.220,14333'
 $CentralDatabase = 'RPMAssure_App'
 $CollectDir = 'C:\RPM-Assure\Sql\customers\HYDRA'
 $LogDir = 'C:\RPM-Assure\Sql\customers\HYDRA\logs'
+$CompanyDatabases = @(
+  'Sysprodb',
+  'Sysprodb1',
+  'SYSPRODeployment',
+  'SysproCompanyC',
+  'SysproCompanyC_SRS',
+  'SysproCompanyD',
+  'SysproCompanyD_SRS',
+  'SysproCompanyH',
+  'SysproCompanyH_SRS'
+)
--- Sql/new-customer/Onboard-Hydrasales.ps1
+++ Sql/new-customer/Onboard-Hydrasales.ps1
@@ -1,11 +1,11 @@
-# Hydrasales (HYDRA) onboard - run ON the Hydrasales SYSPRO SQL server only.
+# Hydrasales (HYDRA) - run ON HydraSRV as HYDRASRV\RPMAdmin (Windows).
 #   powershell -NoProfile -ExecutionPolicy Bypass -File .\Onboard-Hydrasales.ps1
 $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 $one = Join-Path $here 'Install-NewCustomer-OneShot.ps1'
 if (-not (Test-Path $one)) { throw "Missing $one" }
+Write-Host ('Windows user: ' + $env:USERDOMAIN + '\' + $env:USERNAME)
 & powershell.exe -NoProfile -ExecutionPolicy Bypass -File $one `
   -PreCustomerCode 'HYDRA' `
   -PreDisplayName 'Hydrasales' `
-  -PreAuthMode 'sql' `
-  -PreAdminUser 'RPMAdmin' `
-  -PreAdminPwd 'RPM@dm1n2026#'
+  -PreInstanceName 'HydraSRV' `
+  -PreAuthMode 'windows'
