c2f957ab649ebcfb0b5a62f7fca1d5737bde6540
Kirk Sweet <kirk@rpmresources.co.za>
2026-08-18 11:25:50 +0000

    Nav: center System Date, rev, System Time evenly; brand+SQL left

--- App/src/components/portfolio/app-shell.tsx
+++ App/src/components/portfolio/app-shell.tsx
@@ -228,8 +228,8 @@ export function AppShell({
                   <strong>RPM Assure</strong>
                 </span>
               </Link>
+              <HeadsUpDisplay liveSql={hudLive} generatedAt={hudAt} variant="sql" />
             </div>
-            <HeadsUpDisplay liveSql={hudLive} generatedAt={hudAt} variant="sql" />
             <HeadsUpDisplay liveSql={hudLive} generatedAt={hudAt} variant="clock" />
             <div className="dk-header-right">
               <DensityToggle />
